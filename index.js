b8fa178ac18e6e82b92c9c74a668cd92d953dd99
bassem <basem12683@gmail.com>
2023-10-06 14:13:48 +0300

    connect mongo

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+
+// main routes
+// app.use('/api/goals', require('./routes/goalRoutes'));
+// app.use('/api/users', require('./routes/userRoutes'));
+
+//Connect to the database before listening
+connectDB().then(() => {
+  app.listen(port, (err) => {
+      if (err) throw err;
+      console.log('Server listening on port'.red, port.yellow);
+    });      
+})
