529486204053908ae830da18cca3cf32b210f021
bassem <basem12683@gmail.com>
2023-10-13 23:04:47 +0300

    patch

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,6 +1,14 @@
 const asyncHandler = require("express-async-handler");
 const Product = require("../models/productModel");
 
+// @desc    Get products
+// @route   GET /api/products
+// @access  private
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json(products);
+});
+
 // @desc    Set product
 // @route   POST /api/products
 // @access  puplic
@@ -13,6 +21,37 @@ const setProduct = asyncHandler(async (req, res) => {
   res.status(200).json(product);
 });
 
+// @desc    Delete product
+// @route   DELETE /api/products/:id
+// @access  private
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(400).json({ message: "product not found" });
+  }
+
+  await product.deleteOne();
+  res.status(200).json({ id: req.params.id });
+});
+
+// @desc    Update goal
+// @route   PUT /api/goals/:id
+// @access  private
+const updateProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(400).json({ message: 'product not found' });
+  }
+ 
+  const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedProduct);
+});
+
 module.exports = {
+  getProducts,
   setProduct,
+  deleteProduct,
+  updateProduct
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { setProduct } = require("../controllers/productControllers");
+const {
+  getProducts,
+  setProduct,
+  deleteProduct,
+  updateProduct,
+} = require("../controllers/productControllers");
 
-router.route("/").post(setProduct);
-
+router.route("/").get(getProducts).post(setProduct);
+router.route("/:id").delete(deleteProduct).patch(updateProduct);
 
 module.exports = router;
